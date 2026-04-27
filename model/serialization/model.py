2a3136d9b4a4f0825a9389479cd93e838e9c5fd8
HumbertoBPF <humbertobpf@gmail.com>
2026-04-27 01:37:40 -0300

    :bug: Fixing the default UUIDs, which were always evaluating to the same value :bug:

--- model/serialization/model.py
+++ model/serialization/model.py
@@ -1,21 +1,21 @@
-import uuid
 from typing import Annotated
+from uuid import UUID, uuid4
 
-from pydantic import BaseModel, BeforeValidator
+from pydantic import BaseModel, BeforeValidator, Field
 
 from validation.validation import validate_email, validate_password
 
 
 class Link(BaseModel):
-    id: uuid.UUID = uuid.uuid4()
+    id: UUID = Field(default_factory=uuid4)
     name: str
     url: str
     description: str | None = None
-    user_id: uuid.UUID
+    user_id: UUID
 
 
 class User(BaseModel):
-    id: uuid.UUID = uuid.uuid4()
+    id: UUID = Field(default_factory=uuid4)
     first_name: str
     last_name: str
     email: Annotated[str, BeforeValidator(validate_email)]
